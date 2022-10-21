fee04fb269cbd14631d9b7eced4c57db34a7c591
Dario Marquinez <1201dario.m70@gmail.com>
2022-10-21 14:53:05 -0500

    segundo: seguimiento del proyecto

---
+++ servicios/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+
+# Create your models here.
+
+class Servicios(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='servicios')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+
+    def __str__(self):
+        return self.titulo
+
+
+
