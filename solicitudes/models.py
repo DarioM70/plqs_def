5ea23572613f8dda14558360cb26718b13838b9d
Dario Marquinez <1201dario.m70@gmail.com>
2022-11-03 14:02:38 -0500

    login

---
+++ solicitudes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Solicitudes(models.Model):
+    pass
+
+class Asignación(models.Model):
+    pass
+
+class Estadp(models.Model):
+    pass
+
+class reseña(models.Model):
+    pass
+
