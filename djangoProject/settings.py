1883f4583dbb971cfd694456a0ab30ee719d3544
Dario Marquinez <1201dario.m70@gmail.com>
2022-11-25 07:28:38 -0500

    procfile correccion2

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5!cc=xez4vgpg0e%s*x8caqi5k_3uk
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
